801c8ab2b168c8f461f0a9e5f0987fb7fe4a5ebd
Toshi Wakita <wakita@cco.kanagawa-it.ac.jp>
2016-02-12 03:27:39 +0900

    minor fix

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -10,12 +10,12 @@ pip install rospkg
 cd ..
 git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu14.04_server.git
 cd ./ros_setup_scripts_Ubuntu14.04_server
-bash ./step0/bash
-bash ./step1/bash
+bash ./step0.bash
+bash ./step1.bash
 
 #catkin setup
 mkdir -p ~/catkin_ws/src
-cd ~/catkin_ws_src
+cd ~/catkin_ws/src
 source /opt/ros/indigo/setup.bash
 catkin_init_workspace
 cd ~/catkin_ws
